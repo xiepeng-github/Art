38904c91e121a98db24648220c3e190685f5aee7
xiepeng <xpsunshine@163.com>
2018-05-29 17:51:21 +0800

    xiepeng 新建art项目

---
+++ art-parent/art-websocket/src/main/java/com/xp/art/ArtWebSocketApplication.java
@@ -0,0 +1,12 @@
+package com.xp.art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtWebSocketApplication.class, args);
+	}
+}
